8a7e07bcffa33a211443bdfcc42bc443a760781a
LabyKreative <labyreative@yahoo.com>
2022-11-30 03:34:15 -0800

    Added script

---
+++ 0x00-shell_basics/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+path="readlink -f '${BASH_SOURCE:-$0}'"
+DIR_PATH="dirname $path"
+echo "The absolute path is" $path
+echo "------------------------------------"
+echo "The Directory Path is" $DIR_PATH
