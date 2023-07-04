69bb446c87752ae130ee0797adc28e02534291fd
LabyKreative <labykreative@yahoo.com>
2023-07-04 00:56:19 +0000

    Add a custom HTTP header with Puppetsudo git add .!

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
-# a Puppet script that it configures a brand new Ubuntu machine to the requirements asked in this task
+# a Puppet script that it configures a brand new Ubuntu machine
 
-exec {'update_package_list':
+{'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
   before   => Exec['install Nginx'],
@@ -9,17 +9,17 @@ exec {'update_package_list':
 exec {'install Nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx',
-  before   => Exec['header_display'],
+  before   => Exec['add_header'],
 }
 
-exec { 'header_display':
+exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\theader_display X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
   before      => Exec['restart Nginx'],
 }
 
-exec { 'restart Nginx service':
+exec { 'restart Nginx':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
