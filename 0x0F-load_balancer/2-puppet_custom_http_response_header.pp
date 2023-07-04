c164b9319606ed1eb14cb48fd09a42c5e1a76fe7
LabyKreative <labykreative@yahoo.com>
2023-07-04 00:58:54 +0000

    Add a custom HTTP header with Puppetsudo git add...!

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # a Puppet script that it configures a brand new Ubuntu machine
 
-{'update':
+{'update_package_lists':
   provider => shell,
   command  => 'sudo apt-get -y update',
   before   => Exec['install Nginx'],
@@ -9,13 +9,13 @@
 exec {'install Nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
+  before   => Exec['header'],
 }
 
-exec { 'add_header':
+exec { 'header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\theader X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
   before      => Exec['restart Nginx'],
 }
 
