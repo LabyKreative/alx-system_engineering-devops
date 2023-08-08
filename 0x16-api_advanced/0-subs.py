a8bd5b8a47366a022642d0eeba96d94e3e709c12
LabyKreative <labykreative@yahoo.com>
2023-08-08 04:55:48 -0700

    Update: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {"User-agent": "Google Chrome Version 81.0.4044.129"}
+    user_agent = {"User-agent": "Google Chrome Version 115.0.5790.171"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
