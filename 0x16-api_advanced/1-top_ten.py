9bef6e286c621c1ccd7eb9c05460e50251f08cee
LabyKreative <labykreative@yahoo.com>
2023-08-09 21:00:16 -0700

    Update: Top ten programming  posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,17 +7,17 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the 10 hottest posts on a particular subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/LabyKreative)"
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    header = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/LabyKreative)"
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    data = requests.get(url, headers=header, allow_redirects=False)
+
+    if data.status_code == 200:
+        data = data.json().get("data").get("children")
+        for element in data:
+            print(element.get("data").get("title"))
+    else:
+        print(None)
