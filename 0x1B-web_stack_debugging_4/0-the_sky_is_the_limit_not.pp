05809d04fe28a37337f472d21b0307f8cc8f940f
LabyKreative <labykreative@yahoo.com>
2023-08-17 21:29:43 -0700

    Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# puppet scrip to fix the  problem of high amount of requests made
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
